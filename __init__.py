e0050741b75f7d416b0d9db8e6048a09bef96114
ballandt <89742906+ballandt@users.noreply.github.com>
2026-01-11 16:55:10 +0100

    Documentation `__init__` file

--- __init__.py
+++ __init__.py
@@ -2,13 +2,17 @@ import ui
 import database
 import os
 
+# connects to setup database
 setup = database.Setup()
 
+# creates first provider when app is opened for the first time
 if not setup.active_provider():
     ui.NewProviderWindow(setup)
 
+# select active provider for opening screen
 db = setup.active_provider()[0][1]
+# connects provider database
 base = database.Db(os.getcwd() + db)
 
-
+# starts app
 app = ui.MainWindow(base, setup)
